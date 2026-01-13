47fde08916048dfa7ddc4abcf47ce7717c593c0c
Emily C. Wilke <emily.wilke@heigit.org>
2026-01-13 13:57:59 +0100

    feat: add main function for development purposes

---
+++ mobility_tools/main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import shapely
+from detour_factors import get_detour_factors
+from ors_settings import ORSSettings
+
+aoi = shapely.box(8.671217, 49.408404, 8.680658, 49.418400)
+ors_settings = ORSSettings()
+detour_factors = get_detour_factors(aoi=aoi, ors_settings=ors_settings, profile='foot-walking')
+
+fig, ax = plt.subplots(1, 1)
+detour_factors.plot(column='detour_factor', ax=ax)
+
+fig.savefig('test.png')
