158120391108a7acec18c11e14c501f5fd029d23
Emily C. Wilke <emily.wilke@heigit.org>
2026-01-14 16:18:02 +0100

    feat: time stuff

--- mobility_tools/main.py
+++ mobility_tools/main.py
@@ -1,17 +1,27 @@
-import pandas as pd
+from time import time_ns
+
 import shapely
+from detour_factors import get_detour_factors
 from detour_factors_new import get_detour_factors_new
 from ors_settings import ORSSettings
 
 aoi = shapely.box(8.671217, 49.408404, 8.7000658, 49.420400)
 ors_settings = ORSSettings()
 
+start = time_ns()
 detour_factors_new = get_detour_factors_new(aoi=aoi, ors_settings=ors_settings, profile='foot-walking')
+end = time_ns()
+print(f'New: {end - start} ns')
 
-from detour_factors_new import TIMES
+start = time_ns()
+detour_factors_old = get_detour_factors(aoi=aoi, ors_settings=ors_settings, profile='foot-walking')
+end = time_ns()
+print(f'Old: {end - start} ns')
 
-times = pd.DataFrame.from_records(TIMES)
-print(times.sum(axis=0))
+# from detour_factors_new import TIMES
+#
+# times = pd.DataFrame.from_records(TIMES)
+# print(times.sum(axis=0))
 # detour_factors_new = get_detour_factors_new(aoi=aoi, ors_settings=ors_settings, profile='foot-walking')
 
 
