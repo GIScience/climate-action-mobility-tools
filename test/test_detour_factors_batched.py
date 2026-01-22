48833bce853f102ff561ba588d608608d760664e
Emily C. Wilke <emily.wilke@heigit.org>
2026-01-22 13:18:51 +0100

    test(detour_factors_batched): add test_calculate_detour_factors

--- test/test_detour_factors_batched.py
+++ test/test_detour_factors_batched.py
@@ -1,8 +1,14 @@
+import numpy as np
 import pandas as pd
 import shapely
+from pyproj import Transformer
 from vcr import use_cassette
 
-from mobility_tools.detour_factors_batched import extract_coordinates, get_detour_factors_batched
+from mobility_tools.detour_factors_batched import (
+    calculate_detour_factors,
+    extract_coordinates,
+    get_detour_factors_batched,
+)
 
 
 @use_cassette
@@ -34,3 +40,45 @@ def test_extract_coordinates():
 
     assert center == (0.0, 0.0)
     assert corners == expected_corners
+
+
+def test_calculate_detour_factors():
+    # TODO think about how to make this test more understandable, made up coordinates for easy math
+    chunk_distances = [
+        {
+            'distances': [66.4, 73.7, 101.2, 52.4, 72.9, 833.4],
+            'snapped_coordinates': {
+                'center': [8.674202, 49.409577],
+                'corners': [
+                    [8.673291, 49.409591],
+                    [8.67319, 49.409592],
+                    [8.673858, 49.40931],
+                    [8.674926, 49.409588],
+                    [8.675209, 49.409593],
+                    [8.674519, 49.4113],
+                ],
+            },
+        },
+        {
+            'distances': [495.2, 397.8, 416.5, 500.7, 544.8, 495.1],
+            'snapped_coordinates': {
+                'center': [8.677646, 49.409741],
+                'corners': [
+                    [8.676915, 49.410034],
+                    [8.676694, 49.409398],
+                    [8.677398, 49.408886],
+                    [8.678332, 49.409522],
+                    [8.67864, 49.409852],
+                    [8.677954, 49.409818],
+                ],
+            },
+        },
+    ]
+
+    result = calculate_detour_factors(
+        chunk_distances, transform=Transformer.from_crs(crs_from='EPSG:4326', crs_to='EPSG:32632')
+    )
+
+    expected_result = [np.float64(1.8221085635843355), np.float64(9.080471601313818)]
+
+    assert result == expected_result
