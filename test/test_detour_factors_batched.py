d9659b38d7272f954000f3a5dc9a17ab7eb83e57
Emily C. Wilke <emily.wilke@heigit.org>
2026-01-22 10:29:06 +0100

    test(detour_factors_batched): extract coordinates

--- test/test_detour_factors_batched.py
+++ test/test_detour_factors_batched.py
@@ -1,7 +1,8 @@
+import pandas as pd
 import shapely
 from vcr import use_cassette
 
-from mobility_tools.detour_factors_batched import get_detour_factors_batched
+from mobility_tools.detour_factors_batched import extract_coordinates, get_detour_factors_batched
 
 
 @use_cassette
@@ -16,3 +17,20 @@ def test_get_detour_factors_batched(default_ors_settings):
     for detour_factor in result.detour_factor:
         assert isinstance(detour_factor, float)
     assert result.active_geometry_name is not None
+
+
+def test_extract_coordinates():
+    cell_center = shapely.Point(0.0, 0.0)
+    ring = [(1.0, 1.0), (-1.0, 1.0), (-1.0, -1.0), (1.0, -1.0), (1.0, 1.0)]
+    geometry = shapely.Polygon(ring)
+
+    expected_corners = ring.copy()
+    expected_corners.pop()
+
+    row = pd.Series(data=[cell_center, geometry], index=['cell_center', 'geometry'])
+    result = extract_coordinates(row)
+    center = result['center']
+    corners = result['corners']
+
+    assert center == (0.0, 0.0)
+    assert corners == expected_corners
